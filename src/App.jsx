7416ca4825f1ce574cb9d49edf91db9f34ceeadd
abdelrehimzeyad <abdelrehimzeyad@gmail.com>
2025-10-12 18:18:36 +0200

    Initial commit for my music app

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "2rem" }}>
+      <h1>🎶 PlaylistPalette</h1>
+      <p>Welcome to my music sharing website!</p>
+    </div>
+  );
+}
+
+export default App;
